3e83f77da720823955f875dfcb7ccab38b435bcd
nadduli <naddulidaniel94@gmail.com>
2022-06-29 00:46:58 +0300

    create post requests

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
-#from fastapi.params import Body
+from typing import Optional
+from fastapi import FastAPI, Response
+from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
 
 class Post(BaseModel):
     title: str
-    content:str 
+    content:str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "Title for post 1", "content": "content for post 1", "id": 1},
+            {"title": "pirates", "content": "pirates of the caribbean", "id": 2},
+            {"title": "favorite foods", "content": "rice, matooke and meat", "id": 3 }]
+
+def find_post(id: int):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 
 @app.get("/")
 async def root():
@@ -16,9 +31,18 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"message": "retrieve my posts"}
+    return {"message": my_posts}
 
-@app.post("/create_posts")
-def create_posts(new_post : Post):
-    print(new_post)
-    return {"data": "new posts"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"post": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(int(id))
+    if not post:
+        response.status_code = 404
+    return {"data": post}
